9d5b505216f618c4e50b8f01b308f48d26073684
santhoshparamasivam <santhoshparamasivam78@gmail.com>
2025-06-13 22:37:40 +0530

    multi-value form item support added

--- database_schema/dbschema.sql
+++ database_schema/dbschema.sql
@@ -21,7 +21,7 @@ CREATE TABLE "form_items" (
   "id" SERIAL PRIMARY KEY,
   "form_id" integer,
   "input_type" input_type,
-  "form_item_value" varchar,
+  "form_item_value" varchar [],
   "form_item_response" varchar,
   "form_item_user" integer
 );
@@ -38,4 +38,3 @@ ALTER TABLE "form_items" ADD FOREIGN KEY ("form_id") REFERENCES "forms" ("id");
 ALTER TABLE "form_items" ADD FOREIGN KEY ("form_item_user") REFERENCES "users" ("id");
 
 ALTER TABLE "forms" ADD FOREIGN KEY ("admin_id") REFERENCES "admins" ("id");
-
